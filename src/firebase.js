20ecba7d0f0fb62d88868cd27f5971d7a6310a3a
Kai Tore Vik <kaitorevik@hanmedhatten.local>
2026-03-31 23:49:37 +0200

    Lagt inn ekte Firebase konfigurasjon

--- src/firebase.js
+++ src/firebase.js
@@ -1,9 +1,6 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getFirestore } from "firebase/firestore";
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyCsCClf6eJknr2cIiBWSgAqQ_q8LVoyG_k",
   authDomain: "bjerkreim-lyd-lys.firebaseapp.com",
@@ -13,5 +10,5 @@ const firebaseConfig = {
   appId: "1:448986782503:web:70b356c6adba737d342702"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
